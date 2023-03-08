d4dfd7a77ce191d92662a8d39ccd824b407a2e37
AdolfoFGP <adolfo.fgp@gmail.com>
2023-03-07 22:42:37 -0300

    Add count N° of words as input

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,10 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
+
+    lista_text = user_text.split()
+    num_caract = len(lista_text)
+
     return render(request, 'reverse.html', {'usertext':user_text,
-                                            'reversedtext':reversed_text})
+                                            'reversedtext':reversed_text,
+                                            'numcaract':num_caract})
